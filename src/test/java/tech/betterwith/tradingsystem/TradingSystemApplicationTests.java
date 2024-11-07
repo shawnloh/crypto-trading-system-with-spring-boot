039ff88eead44fbf99727c01f8b71cd260115cf8
Shawn Loh <tiensoonshawnloh@gmail.com>
2024-11-07 23:18:48 +0800

    initial commit

---
+++ src/test/java/tech/betterwith/tradingsystem/TradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.betterwith.tradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
